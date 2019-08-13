54970b53ad4f10ab23ca8470271b42a989cd05dd
Patrick M Ritchie <pmritchie@student.fullsail.edu>
2019-08-13 00:41:36 -0400

    added database, tables, migration, MVC

---
+++ app/Http/Controllers/ProjectsController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Project;
+
+use Illuminate\Http\Request;
+
+class ProjectsController extends Controller
+{
+    public function index()
+    {
+        $projects = Project::all();
+
+        
+
+        return view('projects.index', compact('projects'));
+    }
+}
